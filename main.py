7011c1394773ff65ecff2ff3fcc1904910203ef3
kikibase <o.f.d169@gmail.com>
2024-11-03 01:58:42 -0500

    first

---
+++ main.py
@@ -0,0 +1,7 @@
+class particle:
+
+    def __init__(self, particle_name, charged, mass):
+        self.particle_name:str = particle_name
+        self.charged:bool = charged #if particle is charged
+        self.mass:float = mass
+        
